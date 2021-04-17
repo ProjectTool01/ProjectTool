5c3b3bdd1f9667e0ee036eef6eb133405e2ac151
Иван <geriko2000@yandex.ru>
2021-04-18 00:27:08 +0700

    add custom error pages

---
+++ src/main/java/com/example/ProjectTool/controller/ErrorController.java
@@ -0,0 +1,36 @@
+package com.example.ProjectTool.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error/500";
+            }else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error/403";
+            }else if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
+                return "error/401";
+            }else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+                return "error/401";
+            }
+
+        }
+        return "error/error";
+    }
+}
